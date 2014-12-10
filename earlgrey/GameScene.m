3bd3107fedebe2b8f16e203c154cb81014be2f7f
池森　崇 <vantan@ikemoridaka-no-MacBook-Air.local>
2014-12-10 20:08:26 +0900

    sprite.xScaleとsprite.yScaleを共に"1.0"に変更しました。

--- earlgrey/GameScene.m
+++ earlgrey/GameScene.m
@@ -38,8 +38,8 @@
         
         SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
         
-        sprite.xScale = 0.5;
-        sprite.yScale = 0.5;
+        sprite.xScale = 1.0;
+        sprite.yScale = 1.0;
         sprite.position = location;
         
         SKAction *action = [SKAction rotateByAngle:M_PI duration:100];
