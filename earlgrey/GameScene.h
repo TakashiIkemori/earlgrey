a790c1722b8956a803f37b33fd2c1eb84445a091
tanigami <tanigami@gmail.com>
2014-12-10 19:27:51 +0900

    Initial Commit

---
+++ earlgrey/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  earlgrey
+//
+
+//  Copyright (c) 2014 SO14. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ earlgrey/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  earlgrey
+//
+
+//  Copyright (c) 2014 SO14. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
